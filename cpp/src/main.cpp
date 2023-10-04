e5399921dd6e3eb4bafe299c267fb625f9422e03
danieldidiobalsamo <daniel.didiobalsamo@yahoo.com>
2023-10-04 18:26:55 +0200

    feat: dining philosophers using boost fibers

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,8 +1,10 @@
+#include "DiningPhilosophers.hpp"
 #include "ProducerConsumer.hpp"
 #include "ReadersWriters.hpp"
 
 int main() {
   // ProducerConsumer::run();
-  ReadersWriters::run(3, 1);
+  // ReadersWriters::run(3, 1);
+  DiningPhilosophers::run();
   return 0;
 }
